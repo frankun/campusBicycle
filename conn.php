56dc70de974eb3053ee49df28abdf9b0f8ed1615
freekun <836783712@qq.com>
2016-05-21 15:14:13 +0800

    bike

--- conn.php
+++ conn.php
@@ -1,9 +1,9 @@
 <?php
-$con = mysql_connect("URL_SERVER", "USER_NAME", "USER_PASSWORD");
+$con = mysql_connect("w.rdc.sae.sina.com.cn:3307", "jn41zm50w3", " 2h5ix35hlzylmxyzz2yx0jk2y42ixl3zk24yhi2x");
 if (!$con) {
 	die('Could not connect: ' . mysql_error());
 }
-mysql_select_db("app_scubiketest", $con);
+mysql_select_db("app_campuspubbike", $con);
 mysql_query('SET NAMES UTF8') or die("字符集设置错误");
  $siteName="四川大学公益自行车管理系统";
 ?>
