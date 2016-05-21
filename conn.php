8d7c515a10714cb6b3bc7f22b327a23583d0de2c
freekun <836783712@qq.com>
2016-05-21 14:43:19 +0800

    bike

--- conn.php
+++ conn.php
@@ -1,7 +1,6 @@
-<?php  
-
+<?php
    //第一步：链接数据库  
-    $conn=@mysql_connect("w.rdc.sae.sina.com.cn:3307"," k1w5nn401m","5hly3jy444x0hwhh02l1zzjwxmz243lzh0w3ll0h")or die ("mysql链接失败");
+    $conn=@mysql_connect("w.rdc.sae.sina.com.cn:3307","mnwmxnnl5l"," j0wlx20114ky02lzxyhhj2y243xzzl0xzx11l2ji")or die ("mysql链接失败");
    //第二步: 选择指定的数据库，设置字符集  
     @mysql_select_db("bike",$conn) or die ("db链接失败".mysql_error());  
     mysql_query('SET NAMES UTF8')or die ("字符集设置错误");  
